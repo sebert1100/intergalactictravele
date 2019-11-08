1e53b4af69b7dcd5b0b5e9adccc2608cd59a9087
Sarah <Sebert1100@stu.d214.org>
2019-11-08 11:28:01 -0600

    created second UIViewController and connected starImageView

---
+++ intergalactictravele/secondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  secondViewController.swift
+//  intergalactictravele
+//
+//  Created by Sarah Ebert on 11/8/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    @IBOutlet weak var starImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
